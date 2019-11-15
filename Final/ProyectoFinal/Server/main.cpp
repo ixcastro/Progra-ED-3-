93188c260bd8dad8701b593f86a0ee32a7a019f5
ixcastro <ixcastro@ic-itcr.com>
2019-11-15 04:45:25 -0600

    Almost done

--- Final/ProyectoFinal/Server/main.cpp
+++ Final/ProyectoFinal/Server/main.cpp
@@ -194,3 +194,60 @@ int main(int argc, char *argv[])
 ////    return a.exec();
 ////}
 
+
+
+
+////-------------JM---------------//
+//#include <QCoreApplication>
+//#include "avltree.h"
+//#include "avlnode.h"
+//#include "brand.h"
+
+//int main(){
+//    AVLTree *avl = new AVLTree();
+//    avl->insert(new Product(20,"Arroz"));
+//    avl->insert(new Product(45,"Cloro"));
+//    avl->insert(new Product(34,"Frijoles"));
+//    avl->insert(new Product(28,"Jugos"));
+//    avl->insert(new Product(73,"Jamones"));
+//   // avl->insert(new Product(20,"Sal"));
+
+//    avl->show(avl->getRoot());
+//    cout<<"raiz " << avl->getRoot()->getProduct()->getCode()<<endl;
+
+//    avl->deleteAVLData(20);
+//    cout<<endl;
+//    cout<<"-------------"<<endl;
+//    cout<<endl;
+
+//    avl->show(avl->getRoot());
+//    cout<<"raiz " << avl->getRoot()->getProduct()->getCode()<<endl;
+
+
+
+
+//    RNTree *avl = new RNTree();
+//    avl->insert(new Brand(80,"L",1,1));
+//    avl->insert(new Brand(20,"L",1,1));
+//    avl->insert(new Brand(95,"L",1,1));
+//    avl->insert(new Brand(7,"L",1,1));
+//    avl->insert(new Brand(45,"L",1,1));
+//    avl->insert(new Brand(90,"L",1,1));
+//    avl->insert(new Brand(120,"L",1,1));
+//    avl->insert(new Brand(5,"L",1,1));
+//    avl->insert(new Brand(14,"L",1,1));
+
+//    avl->show(avl->getRoot());
+//    cout<<"raiz " << avl->getRoot()->getBrand()->getCode()<<endl;
+
+//    avl->deleteRNData(95);
+//    cout<<endl;
+//    cout<<"-------------"<<endl;
+//    cout<<endl;
+
+//    avl->show(avl->getRoot());
+//    cout<<"raiz " << avl->getRoot()->getBrand()->getCode()<<endl;
+
+
+//    return 0;
+//}
